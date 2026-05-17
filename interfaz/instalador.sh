4cdbf14025dd5e2ac377e60b81e33d5378a905a1
apososgol1000-png <apososgol1000@gmail.com>
2026-05-16 22:19:35 -0600

    Update instalador.sh

--- interfaz/instalador.sh
+++ interfaz/instalador.sh
@@ -4,16 +4,16 @@ echo "   INSTALANDO INTERFAZ DE AEROMAC-OS      "
 echo "=========================================="
 
 # 1. Actualizar el sistema interno de Debian
-sudo apt update
+apt update
 
 # 2. Instalar el entorno gráfico súper ligero (XFCE) y el gestor de ventanas
-sudo apt install -y xfce4 xfce4-goodies lightdm
+apt install -y xfce4 xfce4-goodies lightdm
 
 # 3. Instalar Plank (El "Dock" flotante estilo Mac para poner las apps abajo)
-sudo apt install -y plank
+apt install -y plank
 
 # 4. Instalar herramientas para aplicar temas visuales y transparencias
-sudo apt install -y lxappearance xfce4-clipman-plugin
+apt install -y lxappearance xfce4-clipman-plugin
 
 echo "=========================================="
 echo "¡Componentes visuales estilo Mac listos!"
